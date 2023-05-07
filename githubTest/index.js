8cc463266ef8ea420e62f47c0ccf2aad064838f3
itztrippleA <abdullahiadam.aa@gmail.com>
2023-05-07 09:40:58 +0100

    2023-05-07T09:40:58+01:00

--- githubTest/index.js
+++ githubTest/index.js
@@ -16,16 +16,25 @@ const SOURCE_FILE_PATH7 = "../yarn.lock";
 
 // static method
 
-// const DATE = moment().subtract(34, "d").format();
-// const data = {
-//   date: DATE,
-// };
-// jsonfile.writeFile(FILE_PATH, data, () => {
-//   simpleGit()
-//     .add([FILE_PATH, TEST_FILE_PATH, "./index.js", "./package.json"])
-//     .commit(DATE, { "--date": DATE })
-//     .push();
-// });
+const DATE = moment().subtract(1, "d").format();
+const data = {
+  date: DATE,
+};
+jsonfile.writeFile(FILE_PATH, data, () => {
+  simpleGit()
+    .add([
+      FILE_PATH,
+      SOURCE_FILE_PATH,
+      SOURCE_FILE_PATH2,
+      SOURCE_FILE_PATH3,
+      SOURCE_FILE_PATH4,
+      SOURCE_FILE_PATH5,
+      SOURCE_FILE_PATH6,
+      SOURCE_FILE_PATH7,
+    ])
+    .commit(DATE, { "--date": DATE })
+    .push();
+});
 
 // specific grid method method
 
@@ -51,40 +60,40 @@ const SOURCE_FILE_PATH7 = "../yarn.lock";
 
 //Randomize dynamic auto fill box method
 
-export const makeCommit = (n) => {
-  console.log("was clicked");
-  if (n === 0) return simpleGit().push();
-  //days
-  //columns
-  //starting with 0
-  const x = random.int(27, 29);
-  //   weeks
-  //rows
-  const y = random.int(0, 1);
-  const DATE = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
-  const data = {
-    date: DATE,
-  };
-  console.log(DATE);
-  jsonfile.writeFile(FILE_PATH, data, async () => {
-    await simpleGit()
-      .add([
-        FILE_PATH,
-        SOURCE_FILE_PATH,
-        SOURCE_FILE_PATH2,
-        SOURCE_FILE_PATH3,
-        SOURCE_FILE_PATH4,
-        SOURCE_FILE_PATH5,
-        SOURCE_FILE_PATH6,
-        SOURCE_FILE_PATH7,
-      ])
-      .commit(DATE, { "--date": DATE }, makeCommit.bind(this, --n));
-  });
-};
+// export const makeCommit = (n) => {
+//   console.log("was clicked");
+//   if (n === 0) return simpleGit().push();
+//   //days
+//   //columns
+//   //starting with 0
+//   const x = random.int(27, 29);
+//   //   weeks
+//   //rows
+//   const y = random.int(0, 1);
+//   const DATE = moment()
+//     .subtract(1, "y")
+//     .add(1, "d")
+//     .add(x, "w")
+//     .add(y, "d")
+//     .format();
+//   const data = {
+//     date: DATE,
+//   };
+//   console.log(DATE);
+//   jsonfile.writeFile(FILE_PATH, data, async () => {
+//     await simpleGit()
+//       .add([
+//         FILE_PATH,
+//         SOURCE_FILE_PATH,
+//         SOURCE_FILE_PATH2,
+//         SOURCE_FILE_PATH3,
+//         SOURCE_FILE_PATH4,
+//         SOURCE_FILE_PATH5,
+//         SOURCE_FILE_PATH6,
+//         SOURCE_FILE_PATH7,
+//       ])
+//       .commit(DATE, { "--date": DATE }, makeCommit.bind(this, --n));
+//   });
+// };
 //it starts from 0, so the box or grid count is always less than 1
-makeCommit(10);
+// makeCommit(10);
